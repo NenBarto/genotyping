6046313a9b3bdd125efc7a0ab7a112e2e6a03a5a
nenbar <n.bartonicek@garvan.org.au>
2021-09-06 05:58:18 +1000

    working version

--- 1.map.sh
+++ 1.map.sh
@@ -1,7 +1,8 @@
+conda init 
 conda activate enviroDNA
 
-#get all the files
-#bwa index -p assembled_contigs -a is assembled_contigs.fasta
+#mapping to OTU3 as the most common OTU of length 282, named mhc282
+#bwa index -p mhc282 -a is mhc282.fasta
 
 #to do
 #GATK
@@ -14,13 +15,16 @@ resultsDir="/share/ScratchGeneral/nenbar/projects/Anthony/results"
 mkdir -p $resultsDir
 
 annotationDir="/share/ScratchGeneral/nenbar/projects/Anthony/annotation"
-index=$annotationDir/"assembled_contigs"
+index=$annotationDir/"mhc282"
 logDir="/share/ScratchGeneral/nenbar/projects/Anthony/logs"
 mkdir -p $logDir
 
 cutadapt_minlen=30
 
 #trimming
+revcompDir="$resultsDir/"$projectname".revcomp"
+mkdir -p $revcompDir
+
 trimDir="$resultsDir/"$projectname".trimgalore"
 mkdir -p $trimDir
 bwaDir="$resultsDir/"$projectname".bwa"
@@ -46,26 +50,35 @@ for file in ${files[@]}; do
 
   file1=$inDir/$sampleName"_L001_R1_001.fastq.gz"
   file2=$inDir/$sampleName"_L001_R2_001.fastq.gz"
-  file2rev=$inDir/$sampleName"_L001_R2_001.revcomp.fastq"
-  fileTrim1=$trimDir/$sampleName"_L001_R1_001_val_1.fq.gz"
-  fileTrim2=$trimDir/$sampleName"_L001_R2_001.revcomp_val_2.fq.gz"
+  #file2rev=$revcompDir/$sampleName"_L001_R2_001.revcomp.fastq"
+  fileTrim1=$trimDir/$sampleName"_L001_R1_001_val_1.fq"
+  fileTrim2=$trimDir/$sampleName"_L001_R2_001_val_2.fq"
+  fileTrim1gz=$trimDir/$sampleName"_L001_R1_001_val_1.fq.gz"
+  fileTrim2gz=$trimDir/$sampleName"_L001_R2_001_val_2.fq.gz"
   bamFile="$bwaDir/$sampleName.bam"
   sortedBamFile="$bwaDir/$sampleName.sorted"
   samFile="$bwaDir/$sampleName.sam"
 
-  rev_command="vsearch --fastx_revcomp $file2 --fastqout $file2rev"
-  gz_command="gzip -f $file2rev"
-  trim_line="trim_galore "$file1" "$file2rev".gz -a $FWD -a2 $REVrc --gzip --paired -o $trimDir"
-  mapping_line="bwa mem $index $fileTrim1 $fileTrim2 | samtools view -S -b - -o $bwaDir/$sampleName.bam"
+
+  #rev_command="vsearch --fastx_revcomp $file2 --fastqout $file2rev"
+  trim_line="cutadapt -g $FWD -G $REV --discard-untrimmed -o $fileTrim1 -p $fileTrim2 $file1 $file2"
+  gz_command1="gzip -f $fileTrim1"
+  gz_command2="gzip -f $fileTrim2"
+
+  #set up header for read groups, important or breaks GATK
+  HEADER=`printf @RG%sID:%s%sSM:%s%sPL:ILLUMINA '\\t' $sampleName '\\t' $sampleName '\\t'`
+  mapping_line="bwa mem -R '"${HEADER}"' $index $fileTrim1gz $fileTrim2gz -o $samFile" 
+  sam2bam_line="samtools view -S -b $samFile -o $bwaDir/$sampleName.bam"
   sort_line="samtools sort $bwaDir/$sampleName.bam $sortedBamFile"
   index_line="samtools index $bwaDir/$sampleName.sorted.bam"
-  clean_line="rm -f $bwaDir/$sampleName.bam;rm -f $outDir/$sampleName.sam; rm -f "$file2rev".gz"
+  clean_line="rm -f $bamFile & rm -f $samFile"
 
-  qsub -b y -wd $logDir -j y -N rev$sampleName -R y -pe smp 1 -V $rev_command
-  qsub -b y -hold_jid rev$sampleName -wd $logDir -j y -N gz$sampleName -R y -pe smp 1 -V $gz_command
-  qsub -b y -hold_jid gz$sampleName -wd $logDir -j y -N trim$sampleName -R y -pe smp 1 -V $trim_line
-  qsub -b y -hold_jid trim$sampleName -wd $logDir -j y -N map$sampleName -R y -pe smp 1 -V $mapping_line
-  qsub -b y -hold_jid map$sampleName -wd $logDir -j y -N sort$sampleName -R y -pe smp 1 -V $sort_line
+  qsub -b y -wd $logDir -j y -N trim$sampleName -R y -pe smp 1 -V $trim_line
+  qsub -b y -hold_jid trim$sampleName -wd $logDir -j y -N gz1$sampleName -R y -pe smp 1 -V $gz_command1
+  qsub -b y -hold_jid gz1$sampleName -wd $logDir -j y -N gz2$sampleName -R y -pe smp 1 -V $gz_command2
+  qsub -b y -hold_jid gz2$sampleName -wd $logDir -j y -N map$sampleName -R y -pe smp 1 -V $mapping_line
+  qsub -b y -hold_jid map$sampleName -wd $logDir -j y -N sam2bam$sampleName -R y -pe smp 1 -V $sam2bam_line
+  qsub -b y -hold_jid sam2bam$sampleName -wd $logDir -j y -N sort$sampleName -R y -pe smp 1 -V $sort_line
   qsub -b y -hold_jid sort$sampleName -wd $logDir -j y -N index$sampleName -R y -pe smp 1 -V $index_line
   qsub -b y -hold_jid index$sampleName -wd $logDir -j y -N clean$sampleName -R y -pe smp 1 -V $clean_line
 
